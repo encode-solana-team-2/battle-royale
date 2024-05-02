6cc4bc1aa57cd312509bfe7482dcd7a3062b6c2b
Ekapob Ukritnukun <ThaiGQ@gmail.com>
2024-05-02 04:52:10 -0400

    Transfer tokens to PDA (#14)

--- tests/battle-royale.ts
+++ tests/battle-royale.ts
@@ -3,6 +3,8 @@ import { Program } from "@coral-xyz/anchor";
 import { BattleRoyale } from "../target/types/battle_royale";
 import { PublicKey } from "@solana/web3.js";
 import { expect } from "chai";
+import assert = require("assert");
+import * as spl from '@solana/spl-token';
 
 describe("battle-royale", async () => {
   // Configure the client to use the local cluster.
@@ -34,5 +36,83 @@ describe("battle-royale", async () => {
     let vaultState = await program.account.tokenVault.fetch(tokenVaultPDA);
     expect(vaultState.bonkPrice).to.equal(0.00002308);
     expect(vaultState.wifPrice).to.equal(2.58);
+  });
+
+  it("transferSplTokens", async () => {
+    // Generate keypairs for the new accounts
+    const wallet = new anchor.web3.Keypair;
+    const connection = provider.connection;
+    const fromKp = wallet;
+
+    // Fund the wallet with SOL
+    await provider.connection.confirmTransaction(
+      await provider.connection.requestAirdrop(
+        wallet.publicKey,
+        2 * LAMPORTS_PER_SOL
+      )
+    );
+
+    [tokenVaultPDA] = await anchor.web3.PublicKey.findProgramAddressSync(
+      [provider.publicKey.toBuffer()],
+      program.programId
+    );
+
+    // Create a new mint and initialize it
+    const mintKp = new anchor.web3.Keypair();
+    const mint = await spl.createMint(
+      connection,
+      wallet,
+      fromKp.publicKey,
+      null,
+      0
+    );
+
+    // Create associated token accounts for the new accounts
+    const fromAta = await spl.getOrCreateAssociatedTokenAccount(
+      connection,
+      wallet,
+      mint,
+      fromKp.publicKey
+    );
+    const toAta = await spl.getOrCreateAssociatedTokenAccount(
+      connection,
+      wallet,
+      mint,
+      tokenVaultPDA,
+      true
+    );
+
+    // Mint tokens to the 'from' associated token account
+    const mintAmount = 1000;
+    await spl.mintTo(
+      connection,
+      wallet,
+      mint,
+      fromAta.address,
+      wallet.publicKey,
+      mintAmount
+    );
+
+    // Send transaction
+    const transferAmount = new anchor.BN(500);
+    const txHash = await program.methods
+      .transferSplTokens(transferAmount)
+      .accounts({
+        from: fromKp.publicKey,
+        fromAta: fromAta.address,
+        tokenVault: toAta.address,
+        tokenProgram: spl.TOKEN_PROGRAM_ID,
+      })
+      .signers([wallet])
+      .rpc();
+
+    console.log(`https://explorer.solana.com/tx/${txHash}?cluster=devnet`);
+    await connection.confirmTransaction(txHash, "finalized");
+    const toTokenAccount = await connection.getTokenAccountBalance(toAta.address);
+    assert.strictEqual(
+      toTokenAccount.value.uiAmount,
+      transferAmount.toNumber(),
+      "The 'to' token account should have the transferred tokens"
+    );
   });
 });
